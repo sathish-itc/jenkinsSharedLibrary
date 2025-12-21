3d08821131ada98c3e4799a57d2513903c043410
sathish-itc <krssathish77@gmail.com>
2025-12-21 18:57:16 +0000

    Refactor pushDocker function to use String type

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,9 +1,13 @@
-def call(buildTag) {
-    withCredentials([usernamePassword(credentialsId: 'sathish33', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh """
-            echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
-            docker tag sathish-itc:${buildTag} ${DOCKER_USER}/sathish-itc:${buildTag}
-            docker push ${DOCKER_USER}/sathish-itc:${buildTag}
-        """
+def call(String buildTag) {
+    withCredentials([usernamePassword(
+        credentialsId: 'sathish33',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh '''
+          echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+          docker tag sathish-itc:'"$buildTag"' "$DOCKER_USER"/sathish-itc:'"$buildTag"'
+          docker push "$DOCKER_USER"/sathish-itc:'"$buildTag"'
+        '''
     }
 }
