50c32dc980f5ffdc4409accb365aea362ae927ab
sathish-itc <krssathish77@gmail.com>
2025-12-21 19:07:23 +0000

    Fix variable escaping in pushDocker.groovy

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -4,10 +4,10 @@ def call(String buildTag) {
         usernameVariable: 'DOCKER_USER',
         passwordVariable: 'DOCKER_PASS'
     )]) {
-        sh '''
-          echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-          docker tag sathish-itc:'"$buildTag"' "$DOCKER_USER"/sathish-itc:'"$buildTag"'
-          docker push "$DOCKER_USER"/sathish-itc:'"$buildTag"'
-        '''
+        sh """
+          echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+          docker tag sathish-itc:${buildTag} \$DOCKER_USER/sathish-itc:${buildTag}
+          docker push \$DOCKER_USER/sathish-itc:${buildTag}
+        """
     }
 }
