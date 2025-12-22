57582c0f1025dbdf768019299261fc7ebd47df6d
sathish-itc <krssathish77@gmail.com>
2025-12-22 13:01:17 +0000

    Update pushDocker.groovy

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,13 +1,27 @@
-def call(String buildTag) {
-    withCredentials([usernamePassword(
-        credentialsId: 'sathish33',
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )]) {
+def call(buildTag) {
+    withCredentials([usernamePassword(credentialsId: 'sathish33', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
-          echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
-          docker tag sathish-itc:${buildTag} \$DOCKER_USER/sathish-itc:${buildTag}
-          docker push \$DOCKER_USER/sathish-itc:${buildTag}
+            echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
+            docker tag sathish-itc:${buildTag} ${DOCKER_USER}/sathish-itc:${buildTag}
+            docker push ${DOCKER_USER}/sathish-itc:${buildTag}
         """
     }
 }
+
+
+
+// for minikube 
+
+// def call(String buildTag) {
+//     withCredentials([usernamePassword(
+//         credentialsId: 'sathish33',
+//         usernameVariable: 'DOCKER_USER',
+//         passwordVariable: 'DOCKER_PASS'
+//     )]) {
+//         sh """
+//           echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+//           docker tag sathish-itc:${buildTag} \$DOCKER_USER/sathish-itc:${buildTag}
+//           docker push \$DOCKER_USER/sathish-itc:${buildTag}
+//         """
+//     }
+// }
