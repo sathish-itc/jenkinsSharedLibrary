8bdbfa8717af405edd02a508de19427eb76eab17
sathish-itc <krssathish77@gmail.com>
2025-12-22 12:56:54 +0000

    Update generateTag function to apply buildTag
    Refactor call function to use buildTag and apply changes to deployment.yaml.

--- vars/generateTag.groovy
+++ vars/generateTag.groovy
@@ -1,9 +1,20 @@
-def call(String version) {
-    def date = new Date().format('yyyyMMdd')
-    def buildTag = "${version}-${date}.${env.BUILD_NUMBER}"
-    currentBuild.displayName = buildTag
-    sh "echo BUILD_TAG=${buildTag} > build.env"
-    return buildTag
+def call(buildTag) {
+   
+        sh """
+            sed -i "s/IMAGE_TAG/${buildTag}/g" deployment.yaml
+            kubectl apply -f deployment.yaml
+        """
+   
 }
+
+// for minikube cluster 
+
+// def call(String version) {
+//     def date = new Date().format('yyyyMMdd')
+//     def buildTag = "${version}-${date}.${env.BUILD_NUMBER}"
+//     currentBuild.displayName = buildTag
+//     sh "echo BUILD_TAG=${buildTag} > build.env"
+//     return buildTag
+// }
 
 
