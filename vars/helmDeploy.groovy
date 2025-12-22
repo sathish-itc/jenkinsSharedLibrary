f26879ea911899ff106ba8e85d41cb6cab6e6904
sathish-itc <krssathish77@gmail.com>
2025-12-22 15:33:18 +0000

    Delete vars/helmDeploy.groovy

--- vars/helmDeploy.groovy
+++
@@ -1,8 +0,0 @@
-def call(String release, String namespace, String tag) {
-    sh """
-      helm upgrade --install ${release} ./helm \
-        --namespace ${namespace} \
-        --set image.tag=${tag} \
-        --create-namespace
-    """
-}
